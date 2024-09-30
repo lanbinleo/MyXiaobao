3951ae2fe44b5c2becf7af50fad916a12f06ddee
lanbinleo <lanbinwolf@gmail.com>
2024-10-01 00:54:43 +0800

    feat: add new things into home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,23 @@
 <template>
     <div>
-        <h1>欢迎来到首页</h1>
+        <header class="flex justify-between items-center">
+            <Search></Search>
+            <Avatar></Avatar>
+        </header>
+        <div class="banner-bar mt-5">
+            <HomeBanner></HomeBanner>
+        </div>
+        <!-- <h1>欢迎来到首页</h1>
         <p>{{ message }}</p>
-        <button @click="changeMessage">点击改变消息</button>
+        <button @click="changeMessage">点击改变消息</button> -->
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import Search from '../components/Search.vue';
+import Avatar from '../components/Avatar.vue';
+import HomeBanner from '../components/HomeBanner.vue';
 
 const message = ref('这是首页的消息');
 
