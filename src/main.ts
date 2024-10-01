968db2b42e0183e1790b68f5dcc3b0651e21b401
lanbinleo <lanbinwolf@gmail.com>
2024-10-01 11:04:29 +0800

    chore: import pinia

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
+import piniaPersist  from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
+const pinia = createPinia();
+
+pinia.use(piniaPersist);
+
+
 app.use(router)
 app.mount('#app')
