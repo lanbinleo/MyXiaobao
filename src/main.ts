af962965c7ce96bc56470b9c2ccbe35ea40503be
lanbinleo <lanbinwolf@gmail.com>
2024-10-08 20:53:56 +0800

    chore: system edit

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import piniaPersist  from 'pinia-plugin-persistedstate'
+import Toast from 'vue-toastification';
+
 
 const app = createApp(App)
 
 app.use(router)
+app.use(Toast);
 app.use(createPinia().use(piniaPersist))
 
 app.mount('#app')
