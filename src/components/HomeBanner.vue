24cfd4d5e3462c79421d44e8b6234670af471145
lanbinleo <lanbinwolf@gmail.com>
2024-10-12 17:05:16 +0800

    chore: text edit temporary

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="banner flex w-2/5 rounded-xl bg-cover overflow-hidden relative">
         <div class="left flex w-3/5 pl-8 pt-8 pb-8 flex-col justify-start space-y-1">
-            <h1 class="title text-4xl text-white leading-snug">#2024-2025<br>S1Q1 成绩发布</h1>
-            <p class="sub-title text-lg text-white">详情请查看钉钉手机版</p><br>
-            <button class="text-black bg-white rounded-sm text-md  p-2 w-32">了解更多</button>
+            <h1 class="title text-4xl text-white leading-snug">下午好，霍梓烨！<br>周末愉快~</h1>
+            <p class="sub-title text-lg text-white">BC / EL / CSA / Physics 1</p><br>
+            <button class="text-black bg-white rounded-sm text-md  p-2 w-32">前往日程表</button>
         </div>
         <div class="right w-2/5 z-10 flex-none">
             <!-- <div class="relative inset-0 bg-gradient-to-r from-black via-transparent to-black opacity-10 w-full h-full"></div> -->
